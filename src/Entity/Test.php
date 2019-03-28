3454a96e3b5d7288f09e92f1a1d5f5c868d87729
Alan <m.limaalan@hotmail.fr>
2019-03-28 15:48:09 +0100

    add server

---
+++ src/Entity/Test.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TestRepository")
+ */
+class Test
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?int
+    {
+        return $this->name;
+    }
+
+    public function setName(?int $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
