f21b932e506e51da0947e3c60d411267eb90eec1
bobo935 <37068099+bobo935@users.noreply.github.com>
2019-04-30 01:04:59 +0200

    Update TypePerformanceController.php
    Pas fini, manque détails et vérif.

--- src/Controller/TypePerformanceController.php
+++ src/Controller/TypePerformanceController.php
@@ -16,4 +16,78 @@ class TypePerformanceController extends AbstractController
             'controller_name' => 'typePerformanceController',
         ]);
     }
+
+    protected $entity = 'App\Entity\TypePerformance';
+    /**
+     * Recupere tous les donnees de la table TypePerformance
+     * 
+     * @Rest\View()
+     * @Rest\Get("/typeperformance")
+     */
+    public function getTypePerformance()
+    {
+        $typePerformances = $this->getDoctrine()
+            ->getRepository($this->entity)
+            ->findAll();
+
+        if (!$typePerformances)
+            $this->resourceNotFound();
+        return $typePerformances;
+    }
+
+    /**
+     * Retrieve one resource from the table
+     * 
+     * @Rest\View()
+     * @Rest\Get("/typeperformance/{id}")
+     */
+    public function getOneTypePerformance($id)
+    {
+        $typePerformance = $this->findOne($id);
+
+        if (!$typePerformance)
+            $this->resourceNotFound();
+
+        return $typePerformance;
+    }
+
+    /**
+     * Delete the resource
+     * 
+     * @Rest\View()
+     * @Rest\Delete("/typeperformance/{id}")
+     */
+    public function delete($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $typePerformance = $this->getDoctrine()
+            ->getRepository($this->entity)
+            ->find($id);
+        
+        if($typePerformance)
+        {
+            $em->remove($typePerformance);
+            $em->flush();
+        }
+        else
+            $this->resourceNotFound();
+    }
+
+    /**
+     * Return Error in case of a not found.
+     */
+    protected function resourceNotFound()
+    {
+        throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Resource not found');
+    }
+
+    /**
+     * Return a resource by his id.
+     */
+    protected function findOne($id)
+    {
+        return $this->getDoctrine()
+            ->getRepository($this->entity)
+            ->find($id);
+    }
 }
