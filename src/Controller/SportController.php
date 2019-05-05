1ec4e4a3a7294cf44d503c4abac0fa8f2ca54ad5
bobo935 <37068099+bobo935@users.noreply.github.com>
2019-05-05 14:08:59 +0200

    Update SportController.php

--- src/Controller/SportController.php
+++ src/Controller/SportController.php
@@ -3,98 +3,112 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Request\ParamFetcher;
 
 class SportController extends AbstractController
 {
-    /**
-     * @Route("/sport", name="sport")
-     */
-    public function index()
-    {
-        return $this->render('sport/index.html.twig', [
-            'controller_name' => 'SportController',
-        ]);
-    }
-
     protected $entity = 'App\Entity\Sport';
+    protected $namespaceType = 'App\Form\SportType';
     /**
-     * Retrieve all data from one table
-     * 
-     * @Rest\View()
-     * @Rest\Get("/sport")
-     */
-    public function getSport()
+    * Retrieve all data from one table
+    *
+    * @Rest\View()
+    * @Rest\Get("/sport")
+    */
+    public function getSport(ParamFetcher $paramFetcher)
     {
-        $sports =  $this->getDoctrine()
-            ->getRepository($this->entity)
-            ->findAll();
+    $sports = $this->getDoctrine()->getRepository($this->entity);
+    $qb = $repository->findAllSortBy($paramFetcher->get('sortBy'), $paramFetcher->get('sortOrder'));
+    /* ---------------
+    if ($ = $paramFetcher->get(''))
+    $qb = $repository->filterWith($qb, $, 'entity.');
 
-        if (!$sports)
-            $this->resourceNotFound();
+    if ($textSearch = $paramFetcher->get('textSearch'))
+    $qb = $repository->prepTextSearch($qb, $textSearch);
 
-        return $sports;
+
+    --------------- */
+    $qb = $repository->pageLimit($qb, $paramFetcher->get('page'), $paramFetcher->get('limit'));
+
+    $sports = $qb->getQuery()->getResult();
+
+    if (!$sports)
+    $this->resourceNotFound();
+
+    return $sports;
     }
 
     /**
-     * Retrieve one resource from the table
-     * 
-     * @Rest\View()
-     * @Rest\Get("/sport/{id}")
-     */
+    * Retrieve one resource from the table
+    *
+    * @Rest\View()
+    * @Rest\Get("/sport/{id}")
+    */
     public function getOneSport($id)
     {
-        $sport = $this->findOne($id);
+    $sport = $this->findOne($id);
 
-        if (!$sport)
-            $this->resourceNotFound();
+    if (!$sport)
+    $this->resourceNotFound();
 
-        return $sport;
+    return $sport;
     }
 
-    /**
-     * Resource 
-     */
+
+    /* ------------
+
+    A rajouter :
+    - create & persist a resource in db
+
+    - update complete the resource
+
+    - update partial the resource
+
+    -------------- */
+
+
+
 
     /**
-     * Delete the resource
-     * 
-     * @Rest\View()
-     * @Rest\Delete("/sport/{id}")
-     */
+    * Delete the resource
+    *
+    * @Rest\View()
+    * @Rest\Delete("/sport/{id}")
+    */
     public function delete($id)
     {
-        $em = $this->getDoctrine()->getManager();
-        $sport = $this->getDoctrine()
-            ->getRepository($this->entity)
-            ->find($id);
-        
-        if($sport)
-        {
-            $em->remove($sport);
-            $em->flush();
-        }
-        else
-            $this->resourceNotFound();
+    $em = $this->getDoctrine()->getManager();
+    $sport = $this->getDoctrine()
+    ->getRepository($this->entity)
+    ->find($id);
+    if($sport)
+    {
+    $em->remove($sport);
+    $em->flush();
+    }
+    else
+    $this->resourceNotFound();
     }
 
     /**
-     * Return Error in case of a not found.
-     */
+    * Return Error in case of a not found.
+    */
     protected function resourceNotFound()
     {
-        throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Resource not found');
+    throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Resource not found');
     }
 
     /**
-     * Return a resource by his id.
-     */
+    * Return a resource by his id.
+    */
     protected function findOne($id)
     {
-        return $this->getDoctrine()
-            ->getRepository($this->entity)
-            ->find($id);
+    return $this->getDoctrine()
+    ->getRepository($this->entity)
+    ->find($id);
     }
 
 }
