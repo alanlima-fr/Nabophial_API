c31862fef18b1c85fa621f291b7a18b94c0c3325
DevShimi <hospitalier.philippe@gmail.com>
2019-05-22 16:03:46 +0200

    Update ImgController and Repository
    Add Pagination,page limit and sortby

--- src/Repository/ImgRepository.php
+++ src/Repository/ImgRepository.php
@@ -14,10 +14,45 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ImgRepository extends ServiceEntityRepository
 {
+    private $entity = 'App\Entity\Img';
+
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Img::class);
     }
+
+    public function findAllSortBy($sortBy = 'id', $sortOrder = 'desc')
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder(); // Instanciation de la QueryBuilder
+        $qb->select('entity')->from($this->entity, 'entity');  // SELECT FROM, basic simple  
+        
+        switch ($sortBy)
+        {
+            case 'asc':
+                $sortOrder = 'asc';
+                $qb->orderBy('entity.'.$sortBy, $sortOrder); // On effectue le trie
+                break;
+            default:
+                $qb->orderBy('entity.'.$sortBy, $sortOrder); // On effectue le trie
+                break;
+        }
+        
+        
+        return $qb;
+    }
+
+    public function pageLimit($qb, $page, $limit)
+    {
+        $qb->setFirstResult(($page-1) * $limit);
+
+        if ($limit > 100)
+            $limit = 100;
+    
+        $qb->setMaxResults($limit);
+
+        return $qb;
+    }
+
 
     // /**
     //  * @return Img[] Returns an array of Img objects
