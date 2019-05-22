b2f0c475cdbbb3d3095fac33e1ce21a59e05dd14
na2k <nakkibassoumani@gmail.com>
2019-05-22 12:58:15 +0200

    Update region repository

--- src/Repository/RegionRepository.php
+++ src/Repository/RegionRepository.php
@@ -19,6 +19,66 @@ class RegionRepository extends ServiceEntityRepository
         parent::__construct($registry, Region::class);
     }
 
+    public function findAllSortBy($sortBy = 'id', $sortOrder = 'desc') // par defaut on trie par id par ordre décroissant
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder(); // Instanciation de la QueryBuilder
+        $qb->select('entity')->from($this->entity, 'entity');  // SELECT FROM, basic simple
+
+        // en fonction de ce avec quoi on trie
+        switch ($sortBy)
+        {
+            case 'test':
+                break;
+            default:
+                $qb->orderBy('entity.'.$sortBy, $sortOrder); // On effectue le trie
+                break;
+        }
+        return $qb; // On renvoie la QueryBuilder
+    }
+
+    public function filterWith($qb, $array, $where)
+    {
+        $or = $qb->expr()->orx();
+        $array = explode(',', $array);
+        foreach ($array as $value)
+            $or->add($qb->expr()->eq($where, $value));
+        $qb->andWhere($or);
+
+        return $qb;
+    }
+
+    public function prepTextSearch($qb, $textSearch)
+    {
+        $qb->leftJoin('entity.region', 'tsRegion');
+
+        return $qb = $this->textSearch($qb,
+            array('entity.id', 'entity.name', 'tsRegion.name'),
+            $textSearch
+        );
+    }
+
+    public function textSearch($qb, array $fields, $value)
+    {
+        $or = $qb->expr()->orx();
+        foreach ($fields as $field)
+            $or->add($qb->expr()->like($field, $qb->expr()->literal('%'.$value.'%')));
+        $qb->andWhere($or);
+
+        return $qb;
+    }
+
+    public function pageLimit($qb, $page, $limit)
+    {
+        $qb->setFirstResult(($page-1) * $limit);
+
+        if ($limit > 100)
+            $limit = 100;
+
+        $qb->setMaxResults($limit);
+
+        return $qb;
+    }
+
     // /**
     //  * @return Region[] Returns an array of Region objects
     //  */
