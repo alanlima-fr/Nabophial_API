a4def4bf7bc97debb6e9fb9e257fb2b9e301f45e
Alan <alan@hypecode.com>
2020-07-28 22:05:34 +0200

    Add more test unit to AppUserService

--- tests/Service/AppUserServiceTest.php
+++ tests/Service/AppUserServiceTest.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App\Tests\Service;
 
+use App\Entity\AppUser;
+use App\Exception\NotFoundException;
 use App\Repository\AppUserRepository;
 use App\Representation\Pagination;
 use App\Service\AppUserService;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Prophecy\Prophecy\ObjectProphecy;
 
@@ -19,6 +22,8 @@ class AppUserServiceTest extends TestCase
     private $appUserRepositoryProphecy;
     /** @var Pagination|ObjectProphecy */
     private $paginationProphecy;
+    /** @var AppUser|ObjectProphecy */
+    private $appUserProphecy;
     /** @var AppUserService */
     private $service;
 
@@ -32,11 +37,37 @@ class AppUserServiceTest extends TestCase
         $this->service->getAll();
     }
 
+    public function test_get_one_app_user_not_found_exception(): void
+    {
+        $this->appUserRepositoryProphecy
+            ->find(Argument::type('int'))
+            ->shouldBeCalledOnce()
+            ->willReturn(null);
+
+        $this->expectException(NotFoundException::class);
+
+        $this->service->getUser(0);
+    }
+
+    /**
+     * @throws NotFoundException
+     */
+    public function test_get_one_app_user(): void
+    {
+        $this->appUserRepositoryProphecy
+            ->find(Argument::type('int'))
+            ->shouldBeCalledOnce()
+            ->willReturn($this->appUserProphecy->reveal());
+
+        $this->service->getUser(0);
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
         $this->appUserRepositoryProphecy = $this->prophesize(AppUserRepository::class);
         $this->service = new AppUserService($this->appUserRepositoryProphecy->reveal());
         $this->paginationProphecy = $this->prophesize(Pagination::class);
+        $this->appUserProphecy = $this->prophesize(AppUser::class);
     }
 }
