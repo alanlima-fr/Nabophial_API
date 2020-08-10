488d7e3c1be658b72da82b0ca054d55c1950c435
Alan <alan@hypecode.com>
2020-08-10 21:48:05 +0200

    Add getAge()

--- src/Entity/AppUser.php
+++ src/Entity/AppUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use DateTime;
 use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -332,5 +333,13 @@ class AppUser implements UserInterface, HistoryEntityInterface
     public function isGranted(string $role): bool
     {
         return in_array($role, $this->roles, true);
+    }
+
+    /**
+     * @Annotation\Groups({"app_user"})
+     */
+    public function getAge(): int
+    {
+        return $this->birthday->diff(new DateTime('now'))->y;
     }
 }
